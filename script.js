4185713205dfcb1ce9f23675e4e18be79415145d
Agata Jalowska <agatajalowska@gmail.com>
2020-02-08 19:06:52 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+// Add to list by clicking Add button
+
+function addToList() {
+  var element = document.createElement("li");
+  var inputValue = document.getElementById("listInput").value;
+  var text = document.createTextNode(inputValue);
+  element.appendChild(text);
+
+  if (inputValue === '') {
+    alert("Write something!");
+  } else {
+    document.getElementById("shoppingList").appendChild(element);
+  }
+  document.getElementById("listInput").value = ""; //czyści input
+
+  var button = document.createElement("button");
+  var buttonText = document.createTextNode("-");
+  button.className = "remove";
+  button.appendChild(buttonText);
+  element.appendChild(button);
+
+  for (i = 0; i < remove.length; i++) {
+    remove[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
+
+
+//Add "remove button" to list elements
+
+var myList =document.getElementsByTagName("li");
+var i;
+
+for(i=0; i<myList.length; i++){
+    var button = document.createElement("button");
+    var buttonText= document.createTextNode("-");
+    button.className = "remove";
+    button.appendChild(buttonText);
+    myList[i].appendChild(button);
+}
+
+//Remove from list by click on button
+
+var remove = document.getElementsByClassName("remove");
+var i;
+for (i = 0; i < remove.length; i++) {
+  remove[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
